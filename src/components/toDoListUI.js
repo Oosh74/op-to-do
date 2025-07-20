9161d87dff4e8909c7665621558bebcb4491511b
Jared Usher <usherjared@yahoo.com>
2025-07-20 10:00:52 -0400

    Remove console logs and clean up imports/commented code

--- src/components/toDoListUI.js
+++ src/components/toDoListUI.js
@@ -1,8 +1,6 @@
 const renderList = (newList) => {
-  console.log('new list', newList.getList());
   newList.getList().forEach((item) => {
     for (const property in item) {
-      console.log(item);
       let toDoData = document.createElement('p');
       toDoData.textContent = item[property];
       document.querySelector('body').append(toDoData);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './styles.css';
-// import { ToDoFactory } from './components/todoFactory.js';
 import { ToDoList } from './components/toDoList.js';
 import { ToDoForm } from './components/form.js';
 
 const toDoListInstance = new ToDoList();
 
 ToDoForm(toDoListInstance);
-// console.log(ToDoList.getList());
